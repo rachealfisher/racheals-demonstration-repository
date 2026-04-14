596ae118430ee020efeb7f206d99c0b600dd022e
Racheal Fisher <144643155+rachealfisher@users.noreply.github.com>
2026-04-14 15:39:34 -0700

    Add files via upload
    checking hot dogs

---
+++ hotdogchecker.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 14 15:23:23 2026
+
+@author: regal
+"""
+
+myfood = input("What food is it?")
+if myfood == "hotdog" or myfood == "hot dog":
+    print("This is a hot dog")
+    
+else:
+    print("This is not a hot dog")
